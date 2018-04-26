edfe2b55962621afb9c2f030b01e27d42ad27a81
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-04-26 11:34:58 +0300

    Added interfaces in repository project.

---
+++ GeekCoding/GeekCoding.Repository/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace GeekCoding.Repository.Interfaces
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        IEnumerable<T> FindAll();
+        IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void Create(T entity);
+        T GetItem(Guid id);
+        void Update(T entity);
+        void Delete(T entity);
+        void Delete(Guid entityId);
+        void Save();
+    }
+}
---
+++ GeekCoding/GeekCoding.Repository/RepositoryBase.cs
@@ -0,0 +1,57 @@
+﻿using GeekCoding.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace GeekCoding.Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected dynamic RepositoryContext { get; set; }
+        public RepositoryBase(dynamic repoContext)
+        {
+            RepositoryContext = repoContext;
+        }
+
+        public IEnumerable<T> FindAll()
+        {
+            return RepositoryContext.Set<T>();
+        }
+
+        public IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return RepositoryContext.Set<T>().Where(expression);
+        }
+
+        public void Create(T entity)
+        {
+            RepositoryContext.Set<T>().Add(entity);
+        }
+
+        public T GetItem(Guid id)
+        {
+            return RepositoryContext.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            RepositoryContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            RepositoryContext.Set<T>().Remove(entity);
+        }
+
+        public void Delete(Guid entityId)
+        {
+            RepositoryContext.Set<T>().Remove(RepositoryContext.Set<T>()?.Find(entityId));
+        }
+
+        public void Save()
+        {
+            RepositoryContext.SaveChanges();
+        }
+    }
+}
