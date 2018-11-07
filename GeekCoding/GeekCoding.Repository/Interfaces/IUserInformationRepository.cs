0285205497810e8bf1526a7f4003ceb0be1b6730
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-11-07 17:26:05 +0200

    Added repository for userInformation entity.

---
+++ GeekCoding/GeekCoding.Repository/Interfaces/IUserInformationRepository.cs
@@ -0,0 +1,12 @@
+﻿using GeekCoding.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekCoding.Repository.Interfaces
+{
+    public interface IUserInformationRepository : IRepositoryBase<UserInformation>
+    {
+        UserInformation GetUserInformationByUsername(string username);
+    }
+}
---
+++ GeekCoding/GeekCoding.Repository/UserInformationRepository.cs
@@ -0,0 +1,21 @@
+﻿using GeekCoding.Data.Models;
+using GeekCoding.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeekCoding.Repository
+{
+    public class UserInformationRepository : RepositoryBase<UserInformation>, IUserInformationRepository
+    {
+        public UserInformationRepository(EvaluatorContext repoContext) : base(repoContext)
+        {
+        }
+
+        public UserInformation GetUserInformationByUsername(string username)
+        {
+            return RepositoryContext.UserInformation.Where(usrInf => usrInf.Username == username).FirstOrDefault();
+        }
+    }
+}
