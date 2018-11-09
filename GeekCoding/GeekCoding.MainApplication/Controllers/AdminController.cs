8e1bbfcc273e993c0ca68fb02040c21bafbd76ab
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-11-09 16:15:36 +0200

    Added delete operation.

--- GeekCoding/GeekCoding.MainApplication/Controllers/AdminController.cs
+++ GeekCoding/GeekCoding.MainApplication/Controllers/AdminController.cs
@@ -16,16 +16,17 @@ namespace GeekCoding.MainApplication.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        string[] _rolesNames = {"Member", "Proponent" };
         private IUserInformationRepository _userInformationRepository;
         private UserManager<User> _userManager;
-        private IUserInformationService _userRegistration;
+        private IUserInformationService _userInformationService;
 
         public AdminController(IUserInformationRepository userInformationRepository, UserManager<User> userManager,
                                IUserInformationService userRegistration)
         {
             _userInformationRepository = userInformationRepository;
             _userManager = userManager;
-            _userRegistration = userRegistration;
+            _userInformationService = userRegistration;
         }
 
 
@@ -56,7 +57,7 @@ namespace GeekCoding.MainApplication.Controllers
         {
             if (ModelState.IsValid)
             {
-                var result = await _userRegistration.RegisterUser(userInformation);
+                var result = await _userInformationService.RegisterUser(userInformation);
                 if (result == true)
                 {
                     return RedirectToAction("Index");
@@ -90,7 +91,54 @@ namespace GeekCoding.MainApplication.Controllers
         [HttpGet]
         public IActionResult Delete(string id)
         {
-            return View();
+            var userInformation = _userInformationRepository.GetUserById(id);
+            if(userInformation == null)
+            {
+                //that means we don't have any information in userinformation for this user
+                //assign the id from the user
+                return View(new UserInformation { IdUser = id, Username = User.Identity.Name });
+            }
+            return View(userInformation);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(string idUser)
+        {
+            //if the role is admin, do not delete the user
+            if(String.IsNullOrEmpty(idUser))
+            {
+                //it is not in the user information
+                ModelState.AddModelError("", "You cannot delete an null user");
+                return View();
+            }
+
+            var userToDelete = await _userManager.FindByIdAsync(idUser);
+            var isAdmin = await _userManager.IsInRoleAsync(userToDelete, "Admin");
+            if (isAdmin == true)
+            {
+                ModelState.AddModelError("", "You cannot delete an admin");
+                return RedirectToAction(nameof(Delete), new { id = idUser});
+            }
+
+            //the user is not an admin
+            foreach (var role in _rolesNames)
+            {
+                var isInRole = await _userManager.IsInRoleAsync(userToDelete, role);
+                if(isInRole)
+                {
+                    await _userManager.RemoveFromRoleAsync(userToDelete, role);
+                }
+            }
+
+            //delete from asp.net.users
+            await _userManager.DeleteAsync(userToDelete);
+
+            //delete userinformation
+            var userInformationBasedOnId = _userInformationRepository.GetUserById(idUser);
+            _userInformationRepository.Delete(userInformationBasedOnId);
+            _userInformationRepository.Save();
+
+            return RedirectToAction(nameof(Index));
         }
 
     }
